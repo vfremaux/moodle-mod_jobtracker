41145e73a2a26886290870421ad43862108b1839
valery <valery.fremaux@gmail.com>
2017-02-17 20:01:25 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015011902;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2015011902;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2016052300;
-$plugin->component = 'mod_jobtracker';   // Full name of the plugin (used for diagnostics)
+$plugin->component = 'mod_jobtracker';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '3.1.0 (Build 2015011902)';
+$plugin->release = '3.2.0 (Build 2015011902)';
 $plugin->dependencies = array('local_vflibs' => 2015053000);
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
